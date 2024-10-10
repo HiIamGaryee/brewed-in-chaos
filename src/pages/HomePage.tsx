a6cba8a0d865b9942e4e9bd97644873ab4dfb246
HiIamGaryee <tgyandtpy@gmail.com>
2024-10-11 02:14:04 +0800

    update main page and uplaod coffee package image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,57 @@
 import Layout from "../Layout";
-import { Box, Typography, Button, Stack, Grid } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Stack,
+  Grid,
+  CardContent,
+  Card,
+  Avatar,
+} from "@mui/material";
 import coffeeImg from "../assets/coffee1.png";
+import usq1Img from "../assets/brewed-in-chaos/usq-1.jpeg";
+import usq2Img from "../assets/brewed-in-chaos/usq-2.jpeg";
+import usq3Img from "../assets/brewed-in-chaos/usq-3.jpeg";
+import Png from "../assets/brewed-in-chaos/123.png";
+
+const bestSellerList = [
+  {
+    img: Png,
+    name: "Sumatra Mandheling",
+    code: "SM",
+  },
+  {
+    img: Png,
+    name: "Colombian Supremo",
+    code: "CS",
+  },
+  {
+    img: Png,
+    name: "Jamaican Blue Mountain",
+    code: "JBM",
+  },
+  {
+    img: Png,
+    name: "Kenyan AA",
+    code: "KAA",
+  },
+];
+
+const USPList = [
+  {
+    img: usq1Img,
+    name: "BlockChain Payments",
+  },
+  {
+    img: usq2Img,
+    name: "Exclusive coffee farms",
+  },
+  {
+    img: usq3Img,
+    name: "Subscription Boxes",
+  },
+];
 
 const HomePage = () => {
   return (
@@ -16,7 +67,6 @@ const HomePage = () => {
             alignitem: { xs: "center", md: "start" },
             justifyContent: "center",
             pl: { xs: 0, md: 8 },
-
             order: { xs: 2, md: 1 },
           }}
         >
@@ -110,21 +160,86 @@ const HomePage = () => {
           justifyItems: "center",
           flexDirection: "column",
           p: 4,
+          bgcolor: "#f4eee1",
         }}
       >
-        <Typography variant="h3" fontWeight="bold">
+        <Typography variant="h4" fontWeight="bold" mb={2}>
           Our Selling Point
         </Typography>
-        <Typography variant="h6" px={2}>
+        <Typography px={8} textAlign="center" mb={6}>
           Our cyrpto payments and tokenized rewards cater to tech-savvy
           customers, enhacing user engagement and loyalty.
         </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", width: "auto" }}>
+          <Grid container>
+            {USPList.map((item, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 2,
+                    flexDirection: "row",
+                    alignItems: "center ",
+                  }}
+                >
+                  <Avatar
+                    src={item.img}
+                    sx={{
+                      width: 80,
+                      height: 80,
+                      border: (theme) =>
+                        `1px solid ${theme.palette.text.secondary}`,
+                    }}
+                  />
+                  <Typography>{item.name}</Typography>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>{" "}
+        </Box>
       </Box>
-      <Grid container>
-        <Grid item xs={12} md={4}></Grid>
-        <Grid item xs={12} md={4}></Grid>
-        <Grid item xs={12} md={4}></Grid>
-      </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          justifyItems: "center",
+          flexDirection: "column",
+          p: "40px",
+          bgcolor: "#e8e7e5",
+        }}
+      >
+        <Typography variant="h4" fontWeight="bold" mb={6}>
+          Best Seller
+        </Typography>
+
+        <Grid container spacing={4}>
+          {bestSellerList.map((item, index) => (
+            <Grid item xs={12} sm={6} md={3} key={item.code}>
+              <Card sx={{ mb: 2 }}>
+                <CardContent>
+                  <Box
+                    component="img"
+                    src={item.img}
+                    alt="Best Seller Image"
+                    sx={{
+                      cursor: "pointer",
+                      mt: { xs: 8, md: 0 },
+                      p: 1,
+                      width: 1,
+                      height: "auto",
+                      objectFit: "cover",
+                    }}
+                  />
+                </CardContent>
+              </Card>
+              <Typography variant="body1" textAlign="center" sx={{ mb: 2 }}>
+                {item.name}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 };
